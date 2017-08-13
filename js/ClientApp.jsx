e2f4d3e8e177d96cb02097ff151287a0266f0649
Mark Talbot <mark.talbot@experiencepoint.com>
2017-08-13 16:59:13 -0400

    Adding basic routing

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,30 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Landing from './Landing';
+import Search from './Search';
 
-const MyTitle = function(props) {
-    console.log(props);
-    const style = { color: props.color };
-
+const App = () => {
     return (
-        <div>
-            <p style={style}>{props.title}</p>
-        </div>    
+        <BrowserRouter>
+            <div className="app">
+                <Route exact path="/" component={Landing} />
+                <Route exact path="/search" component={Search} />
+            </div>
+        </BrowserRouter>
     );
-}
+};
 
-const MyFirstComponent = function() {
-    return (
-        <div id="my-foobar">
-            <h1>Helllllooooo World!</h1>
-            <MyTitle title="Game of Thrones" color="YellowGreen" />
-            <MyTitle title="Stranger Things" color="GreenYellow" />
-            <MyTitle title="The Fall" color="LimeGreen" />
-            <MyTitle title="The Office (US)" color="peru" />
-        </div>
-    );
-}
-
-render(
-    <MyFirstComponent />,
-    document.getElementById('app')
-);
+render(<App />, document.getElementById('app'));
---
+++ js/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <h3 style={ {color: '#fff'} }>Hi there this is search!</h3>
+    );
+};
+
+export default Search;
