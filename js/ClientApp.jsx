e5f17ddd166209aec323e70a8ddfd5eee390c13b
Mark Talbot <mark.talbot@experiencepoint.com>
2017-08-13 18:40:19 -0400

    Adding TV show data

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
+
+const Error404 = () => {
+    return (
+        <h1>404 - Page not found!</h1>
+    );
+};
 
 const App = () => {
     return (
         <BrowserRouter>
             <div className="app">
-                <Route exact path="/" component={Landing} />
-                <Route exact path="/search" component={Search} />
+                <Switch>
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/search" component={Search} />
+                    <Route component={Error404} />
+                </Switch>
             </div>
         </BrowserRouter>
     );
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import preload from '../data.json';
 
 const Search = () => {
+    console.log(preload);
     return (
-        <h3 style={ {color: '#fff'} }>Hi there this is search!</h3>
+        <div className="search">
+            <pre>{preload.shows.map((show, index) => {
+                return (
+                    <div key={index} className="show-card">
+                        <img src={`/public/img/posters/${show.poster}`} alt={`${show.title} Show Poster`}/>
+                        <div>
+                            <h3>{show.title}</h3>
+                            <h4>({show.year})</h4>
+                            <p>{show.description}</p>
+                        </div>
+                    </div>
+                );
+            })}</pre>
+        </div>
     );
 };
 
