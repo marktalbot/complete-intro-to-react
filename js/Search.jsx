d6731c6aa4638fdc65a4eaa396012d8b0fb9d5ca
Mark Talbot <markjtalbot@gmail.com>
2017-08-15 20:33:38 -0400

    Extracting ShowCard component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import preload from '../data.json';
+import ShowCard from './ShowCard';
 
 const Search = () => {
     console.log(preload);
     return (
         <div className="search">
-            <pre>{preload.shows.map((show, index) => {
-                return (
-                    <div key={index} className="show-card">
-                        <img src={`/public/img/posters/${show.poster}`} alt={`${show.title} Show Poster`}/>
-                        <div>
-                            <h3>{show.title}</h3>
-                            <h4>({show.year})</h4>
-                            <p>{show.description}</p>
-                        </div>
-                    </div>
-                );
-            })}</pre>
+            {preload.shows.map((show, index) => <ShowCard key={index} show={show} />)}
         </div>
     );
 };
---
+++ js/ShowCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ShowCard = (props) => {
+    return (
+        <div className="show-card">
+            <img src={`/public/img/posters/${props.show.poster}`} alt={`${props.show.title} Show Poster`}/>
+            <div>
+                <h3>{props.show.title}</h3>
+                <h4>({props.show.year})</h4>
+                <p>{props.show.description}</p>
+            </div>
+        </div>       
+    );
+}
+
+export default ShowCard;
