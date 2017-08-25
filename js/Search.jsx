2902d31b5f6b9e151d1c2ffd09d1076f9f417a97
Mark Talbot <markjtalbot@gmail.com>
2017-08-24 21:52:11 -0400

    Changing Search from stateless component to stateful component

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,13 +2,40 @@ import React from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
-const Search = () => {
-    console.log(preload);
-    return (
-        <div className="search">
-            {preload.shows.map((show) => <ShowCard key={show.imdbID} show={show} />)}
-        </div>
-    );
-};
+class Search extends React.Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            searchTerm: 'foobarbaz',
+        };
+
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+    }
+
+    onChangeHandler(event) {
+        this.setState({ searchTerm: event.target.value }); // Use setState() method to modify state
+    }
+
+    render() {
+        return (
+            <div className="search">
+                <header>
+                    <h1>svideo</h1>
+                    <input 
+                        onChange={this.onChangeHandler} 
+                        value={this.state.searchTerm} 
+                        type="text" 
+                        placeholder="search" 
+                    />
+                </header>
+                <div>
+                    {preload.shows.map((show) => <ShowCard key={show.imdbID} show={show} />)}
+                </div>
+            </div>
+        );    
+    }
+}
 
 export default Search;
