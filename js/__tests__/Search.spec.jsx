ead0f2cf24a6de7d4423bf9324c6fa1622d6c13f
Mark Talbot <mark.talbot@experiencepoint.com>
2017-09-03 12:55:42 -0400

    Adding test for number of search results based on search term

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -15,6 +15,19 @@ test('Search renders all shows if no search term is provided', () => {
     expect(component.find(ShowCard).length).toEqual(preload.shows.length); // Find the ShowCard child componenets (http://airbnb.io/enzyme/docs/api/ReactWrapper/find.html)
 });
 
-xtest('Search renders the correct number of shows on search term', () => {
+test('Search renders the correct number of shows on search term', () => {
+    const searchTerm = 'black';
+    const component = shallow(<Search />);
 
+    // 1. Finding <input> via CSS selector
+    // 2. Simulation entering text into input and triggering the "onChange" event
+    component.find('input').simulate('change', { target: { value: searchTerm }});
+    
+    const showCount = preload.shows.filter((show) => {
+        return `${show.title} ${show.description}`
+            .toUpperCase()
+            .includes(searchTerm.toUpperCase());
+    }).length;
+
+    expect(component.find(ShowCard).length).toEqual(showCount);
 });
