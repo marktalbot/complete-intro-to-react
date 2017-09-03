477e0477df726daa861dd0f5d6ef2214ae6d99d7
Mark Talbot <mark.talbot@experiencepoint.com>
2017-09-03 11:39:20 -0400

    Setting up shallow enzyme test

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer'; // Can't use "enzyme" and "renderer" together
 import Search from '../Search';
+import { shallow } from 'enzyme'; // Prevents the test from failing if child components change. Stubs out all child components
 
 test('Search renders correctly', () => {
-    const component = renderer.create(<Search />);
-    const tree = component.toJSON();
-
-    expect(tree).toMatchSnapshot();
+    const component = shallow(<Search />);
+    expect(component).toMatchSnapshot();
 });
